d509700d76517147941eb558fb3c6009d120ace8
Matthew Day <Daystar.1998@yahoo.com>
2019-11-17 20:03:03 -0600

    Add header comment

--- Hashed File Processor/Hashed File Processor/hashFile.cpp
+++ Hashed File Processor/Hashed File Processor/hashFile.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+	Name: hashFile.cpp
+
+	Des:
+		This program will read a hashed file ("pinsAndKeys.dat") and a pin file
+		("pins.txt"). It will output to "hashReport.txt." The output will
+		include the pin, the key, and the number of accesses required to find
+		the key or determine it does not exist. It will also output the average
+		number of accesses for the pin.
+
+	Author: Matthew Day
+
+	Due Date: 12/5/2019
+******************************************************************************/
+
 #include <fstream>
 
 using namespace std;
